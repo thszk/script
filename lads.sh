f4c6ce37768088ddd624fa64ce115084d6a334c9
Thiago S <31439064+thszk@users.noreply.github.com>
2019-05-20 10:35:46 -0400

    ..

--- lads.sh
+++ lads.sh
@@ -4,18 +4,31 @@ if [ "$USER" != "root" ]; then
     echo "Without execute permission" && exit 1
 fi
 
-# ---
 
+echo "################ UPDATE ################"
 apt update
-apt upgrade
 
+echo "################ UPGRADE ################"
+apt upgrade -y
+
+echo "################ GIT ################"
 apt install git -y
 
+echo "################ SNAP ################"
 apt install snapd -y
+
+echo "################ VSCODE ################"
 snap install code --classic
 
+echo "################ CURL ################"
+apt install curl -y
+
+echo "################ NODE ################"
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
+
+echo "################ BUILD ESSENTIAL ################"
 apt-get install -y build-essential
 
+echo "################ VUE ################"
 npm install -g @vue/cli
