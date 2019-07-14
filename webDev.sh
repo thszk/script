4f2a74a0ef98f14f484c117b58b56f8738bf60c9
Thiago S <31439064+thszk@users.noreply.github.com>
2019-07-13 23:04:08 -0400

    updates

--- webDev.sh
+++ webDev.sh
@@ -17,16 +17,21 @@ apt install git -y
 echo "################ SNAP ################"
 apt install snapd -y
 
-echo "################ VSCODE ################"
-snap install code --classic
+# echo "################ VSCODE ################"
+# snap install code --classic
+
+echo "################ ATOM ################"
+snap install atom --classic
 
 echo "################ ESLINT EXTENSION ################"
-code --install-extension dbaeumer.vscode-eslint
+# code --install-extension dbaeumer.vscode-eslint
+apm install linter-eslint
 
 echo "################ CURL ################"
 apt install curl -y
 
 echo "################ NODE ################"
+# https://nodejs.org/en/download/package-manager/
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
 
@@ -36,9 +41,12 @@ apt-get install -y build-essential
 echo "################ VUE ################"
 npm install -g @vue/cli
 
-echo "################ POSTMAN ################"
-snap install postman
+# echo "################ POSTMAN ################"
+# snap install postman
 
-echo "################ GITKRAKEN ################"
-snap install gitkraken
+echo "################ INSOMNIA ################"
+snap install insomnia
+
+# echo "################ GITKRAKEN ################"
+# snap install gitkraken
 
